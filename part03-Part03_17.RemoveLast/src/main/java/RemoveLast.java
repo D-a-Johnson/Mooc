b28b77bdee3a44655a7e70523bfea40dd6b77659
D-a-Johnson <johnson.a.derek.94@gmail.com>
2021-12-01 10:51:58 +0100

    exercises

--- part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -5,6 +5,13 @@ public class RemoveLast {
 
     public static void main(String[] args) {
         // Try your method in here
+        ArrayList<Integer> strings = new ArrayList<>();
     }
-
+    public static void removeLast(ArrayList<String> strings) {
+        if (strings.size() == 0) {
+            
+        } else {
+            strings.remove(strings.size() - 1);
+        }
+    }
 }
