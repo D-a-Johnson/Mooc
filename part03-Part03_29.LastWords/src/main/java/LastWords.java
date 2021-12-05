a1525212fe9f7ea4845a6a9a15eef0835cae1d78
D-a-Johnson <johnson.a.derek.94@gmail.com>
2021-12-06 00:00:41 +0100

    check

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class LastWords {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine();
+            String input = scan.nextLine();
             if (input.equals("")) {
                 break;
             }
