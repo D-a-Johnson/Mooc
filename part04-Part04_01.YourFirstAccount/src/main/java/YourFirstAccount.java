f62957c3e23369fc3161cdf59159760cde277263
D-a-Johnson <johnson.a.derek.94@gmail.com>
2021-12-10 01:05:47 +0100

    commits

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,6 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
-        
+        Account derekAccount = new Account("Derek's accout", 100.00);
+        derekAccount.deposit(20);
+        System.out.println(derekAccount);
     }
 }
