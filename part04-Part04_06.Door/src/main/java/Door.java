e0c6b35ffa46033046432f9aa7a1f7cbb377e965
D-a-Johnson <johnson.a.derek.94@gmail.com>
2021-12-27 13:40:52 +0100

    updates

---
+++ part04-Part04_06.Door/src/main/java/Door.java
@@ -0,0 +1,10 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Door {
+    public Door(){
+    }
+
+    public void knock(){
+        System.out.println("Who's there?");
+    }
+}
